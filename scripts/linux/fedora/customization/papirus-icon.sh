a578b994c13fccff67dd7c07dcd826260b0309bd
ponfertato <ponfertato@ya.ru>
2023-08-17 17:36:04 +0300

    themes: move to customization, clear shell theme

---
+++ scripts/linux/fedora/customization/papirus-icon.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+print_block "Fedora - Customization/Papirus Icon"
+
+packages=(
+	papirus-icon-theme
+)
+install_or_update_packages
+
+gsettings set org.gnome.desktop.interface icon-theme 'Papirus'
--- scripts/linux/fedora/themes/papirus-icon.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-print_block "Fedora - Themes/Papirus Icon"
-
-packages=(
-	papirus-icon-theme
-)
-install_or_update_packages
