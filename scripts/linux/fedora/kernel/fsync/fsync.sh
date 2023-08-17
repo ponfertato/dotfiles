ead21b2f8f4f7074f6775ceef23b09f08b322c32
ponfertato <ponfertato@ya.ru>
2023-08-17 17:46:33 +0300

    kernel: added check repository and disabling

--- scripts/linux/fedora/kernel/fsync/fsync.sh
+++ scripts/linux/fedora/kernel/fsync/fsync.sh
@@ -2,6 +2,10 @@
 
 print_block "Fedora - Kernel/Fsync"
 
+if dnf repolist | grep -q "rmnscnce/kernel-xanmod"; then
+    sudo dnf config-manager --disable rmnscnce/kernel-xanmod
+fi
+
 if ! dnf repolist | grep -q "sentry/kernel-fsync"; then
     sudo dnf copr enable sentry/kernel-fsync
 fi
--- scripts/linux/fedora/kernel/xanmod/xanmod-edge.sh
+++ scripts/linux/fedora/kernel/xanmod/xanmod-edge.sh
@@ -2,6 +2,10 @@
 
 print_block "Fedora - Kernel/Xanmod Edge"
 
+if dnf repolist | grep -q "sentry/kernel-fsync"; then
+    sudo dnf config-manager --disable sentry/kernel-fsync
+fi
+
 if ! dnf repolist | grep -q "rmnscnce/kernel-xanmod"; then
     sudo dnf copr enable rmnscnce/kernel-xanmod
 fi
