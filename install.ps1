ba59546325d051600f4192faa617708f29d5ff1b
ponfertato <ponfertato@ya.ru>
2023-10-19 14:32:23 +0300

    💥scripts: add win version checks
    ...soon add more powershell examples

--- install.ps1
+++ install.ps1
@@ -13,12 +13,21 @@ Set-Location "$BASEDIR"
 git -C "$DOTBOT_DIR" submodule sync --quiet --recursive
 git submodule update --init --recursive "$DOTBOT_DIR"
 
-foreach ($PYTHON in ('python', 'python3', 'python2')) {
-	if (& { $ErrorActionPreference = "SilentlyContinue"
-			![string]::IsNullOrEmpty((&$PYTHON -V))
-			$ErrorActionPreference = "Stop" }) {
-		&$PYTHON $(Join-Path "$BASEDIR" -ChildPath "$DOTBOT_DIR" | Join-Path -ChildPath "$DOTBOT_BIN") -d "$BASEDIR" --plugin-dir $DOTBOT_PLUGIN/* -c "$CONFIG$SUFFIX" "$Args"
-		return
-	}
+$PYTHON = ('python', 'python3', 'python2') | where-object { & { $ErrorActionPreference = "SilentlyContinue"; ![string]::IsNullOrEmpty((&$_ -V)); $ErrorActionPreference = "Stop" } } | select-object -first 1
+
+$osVersion = [System.Environment]::OSVersion.Version.Build
+$firstTwoDigits = $osVersion.ToString().substring(0, 2)
+if ($firstTwoDigits -eq "19") {
+	Write-Host "Windows 10"
+	. "${BASEDIR}/scripts/windows/ten.ps1"
+	&$PYTHON $(Join-Path "$BASEDIR" -ChildPath "$DOTBOT_DIR" | Join-Path -ChildPath "$DOTBOT_BIN") -d "$BASEDIR" --plugin-dir $DOTBOT_PLUGIN/* -c "$CONFIG$SUFFIX" "$Args"
+}
+elseif ($firstTwoDigits -eq "22") {
+	Write-Host "Windows 11"
+	. "${BASEDIR}/scripts/windows/eleven.ps1"
+	&$PYTHON $(Join-Path "$BASEDIR" -ChildPath "$DOTBOT_DIR" | Join-Path -ChildPath "$DOTBOT_BIN") -d "$BASEDIR" --plugin-dir $DOTBOT_PLUGIN/* -c "$CONFIG$SUFFIX" "$Args"
+}
+else {
+	Write-Host "unknown windows osVersion: $osVersion, not doing anything special"
 }
 Write-Error "Error: Cannot find Python."
